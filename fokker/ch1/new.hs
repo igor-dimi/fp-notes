72e199b353168fd5db25493e75f76204b5483064
Igor Dimitrov <igor-dimi@protonmail.com>
2026-05-19 18:22:10 +0200

    small modificaiton goffer ch1 code

--- fokker/ch1/new.hs
+++ fokker/ch1/new.hs
@@ -2,3 +2,8 @@ fac n = product [1..n]
 choose n k = fac n / (fac k * fac (n - k))
 n ^^ k = choose n k
 -- comment --
+
+fac2 n  | n == 0    = 1
+        | n > 0     = n * fac(n - 1)
+
+threecopy x = [x, x, x]
